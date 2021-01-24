a098b0b34e5e2b3cb00bdaec9abfec1dac213c1e
RamArvinth <s.p.ramarvinth@gmail.com>
2021-01-24 21:40:00 +0100

    added list checkbox with toggle

--- ToDoList.js
+++ ToDoList.js
@@ -6,13 +6,22 @@ AddNewTaskToList = () => {
     if (document.querySelector("#task-input-box").value.length > 0) {
         let NewTaskList = document.createElement("li")
         NewTaskList.classList.add("list-item")
+
+        let NewCheckbox = document.createElement("input")
+        NewCheckbox.setAttribute("type", "checkbox")
+        NewCheckbox.classList.add("checkbox")
+
         NewTaskList.innerText = document.querySelector("#task-input-box").value
+
 
         NewTaskList.addEventListener("click", (clickEvent) => {
             clickEvent.currentTarget.classList.toggle("task-completed")
+            document.querySelectorAll(" .list-item.task-completed").forEach(item => item.children[0].checked = true)
+            document.querySelectorAll(".list-item:not(.task-completed)").forEach(item => item.children[0].checked = false)
         })
 
         document.querySelector(".task-list").appendChild(NewTaskList)
+        document.querySelector(".task-list .list-item:last-child").prepend(NewCheckbox)
         document.querySelector("#task-input-box").value = null
     }
     else {
